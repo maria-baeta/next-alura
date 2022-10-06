2e4abbe689b4bfe4166979d46dbe70830618c300
maria_baeta <maria.e.martins@somoseducacao.com.br>
2022-10-06 20:17:35 -0300

    feat: Criando as páginas iniciais

---
+++ pages/faq.js
@@ -0,0 +1,14 @@
+import Link from "../src/components"
+
+const FAQPage = () => {
+  return (
+    <div>
+      <h1>
+        Alura Cases - Página de perguntas FAQ
+      </h1>
+      <Link href='/'>Ir para a Home</Link>
+    </div>
+  )
+}
+
+export default FAQPage
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
+import Link from "../src/components"
+
 const HomePage = () => {
   return (
     <div>
       <h1>
         Alura Cases - Home
       </h1>
+      <Link href='/faq'>Ir para o FAQ</Link>
     </div>
   )
 }
