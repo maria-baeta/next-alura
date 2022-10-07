24ce116dfbc7fd7e27b55506df633cdbe6895e73
maria_baeta <maria.e.martins@somoseducacao.com.br>
2022-10-06 22:53:31 -0300

    fix: Limpando residos

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react"
 import Link from "../src/components/Link"
-
 
 export async function getStaticProps(context) {
   const FAQ_API = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
