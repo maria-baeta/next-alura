630aa512db5ad8779dd4745b2d52aea47aff41fe
maria_baeta <maria.e.martins@somoseducacao.com.br>
2022-10-06 22:38:04 -0300

    fix: Jeito next de fazer resquisção a API

--- pages/faq.js
+++ pages/faq.js
@@ -1,21 +1,19 @@
 import { useEffect, useState } from "react"
 import Link from "../src/components/Link"
 
-const FAQPage = () => {
-  const [questions, setQuestions] = useState([])
 
-  useEffect(() => {
-    const FAQ_API = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+export async function getStaticProps(context) {
+  const FAQ_API = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+  const response = await fetch(FAQ_API)
+  const questions = await response.json()
+  return {
+    props: {
+      questions,
+    }
+  }
+}
 
-    fetch(FAQ_API)
-      .then((responseData) => { return responseData?.json() })
-      .then((response) => {
-        setQuestions(response)
-      })
-
-  }, [])
-
-
+const FAQPage = ({ questions }) => {
   return (
     <div>
       <h1>
